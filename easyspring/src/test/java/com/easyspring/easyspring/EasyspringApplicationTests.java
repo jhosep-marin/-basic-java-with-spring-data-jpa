03b06b10f6b2732f5f1c3364c87492a8dc0c4803
Jhosep Marin <marinjhosep15@gmail.com>
2021-04-30 03:12:23 -0500

    Adds initial project's settings

---
+++ easyspring/src/test/java/com/easyspring/easyspring/EasyspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyspring.easyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
